6915d20311575178fb9fd88bd9ba3bcb7c838dbd
robinyeon <robin.yeon@gmail.com>
2022-03-18 23:35:13 +0900

    updated: Installed nodemon and practiced GET request and response

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.end();
+};
+app.get("/", handleHome);
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here!" });
+};
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
